15b6242ce1e8951c32805126640f2a14918c6621
mryuv1 <mryuv1@gmail.com>
2020-12-10 18:29:58 +0200

    another fashla

--- scrape_news.sh
+++ scrape_news.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-wget -qO- https://www.ynetnews.com/category/3082 | grep -E -o '|(https://www.ynetnews.com/article/).........' \
-| uniq >filtered # this command filter only the url's of the articles and save them in file.
+wget -qO- https://www.ynetnews.com/category/3082 | grep \
+-E -o -i 'https://www.ynetnews.com/article/[a-z0-9]{9,10}' | uniq >filtered
 
-cat filtered | wc -l >> results.csv #saves the number of url's in the results.
+  # this command filter only the url's of the articles and save them in file.
 
-#for each ur'l we check how many times 'Netanyahu' and 'Gantz' appeared', and save the results the url.
+cat filtered | wc -l >> results.csv 
+
+#saves the number of url's in the results.
 
 while IFS= read -r line;
 do
@@ -16,3 +18,6 @@ if [[($bibi -eq 0) && ($benny -eq 0)]]; then echo "-" >> results.csv
 else
 echo "Netanyahu, $bibi, Gantz, $benny" >> results.csv; fi;
 done<filtered
+
+#for each ur'l we check how many times 
+#'Netanyahu' and 'Gantz' appeared', and save the results the url.
